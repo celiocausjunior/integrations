b93acfd5fc93fe1592d163675ae24f3060326df6
Celio Caus Junior <celiocausjunior@gmail.com>
2021-01-06 22:15:07 -0300

    API Configuration

--- src/main/java/com/celiocausjr/integrations/resources/EmailResource.java
+++ src/main/java/com/celiocausjr/integrations/resources/EmailResource.java
@@ -1,5 +1,6 @@
 package com.celiocausjr.integrations.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,14 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.celiocausjr.integrations.dto.EmailDTO;
+import com.celiocausjr.integrations.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailResource {
-	
+
+	@Autowired
+	private EmailService emailService;
+
 	@PostMapping
-	public ResponseEntity<Void> send(@RequestBody EmailDTO dto){
-				
+	public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+		emailService.sendEmail(dto);
 		return ResponseEntity.noContent().build();
 	}
 
